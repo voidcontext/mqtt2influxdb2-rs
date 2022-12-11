5167b169a8d9cf70bc7e61f8391d677ca8de36ce
Gabor Pihaj <gabor.pihaj@gmail.com>
2022-12-11 21:00:22 +0000

    Introduce app state, so that config can be reloaded in runtime (later)

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,9 @@
+use async_mutex::Mutex;
 use futures::stream::StreamExt;
 use influxdb2::models::DataPoint;
 use mqtt::Message;
 use paho_mqtt as mqtt;
-use std::{process, time::Duration};
+use std::{process, sync::Arc, time::Duration};
 
 use config::{Config, MqttConfig};
 
@@ -61,31 +62,67 @@ fn to_data_point(msg: &Message, config: &MqttConfig) -> DataPoint {
         .unwrap()
 }
 
+struct AppState {
+    config: Config,
+    influxdb_client: influxdb2::Client,
+    mqtt_client: mqtt::AsyncClient,
+}
+
+impl AppState {
+    fn new(config: Config) -> Self {
+        let create_opts = mqtt::CreateOptionsBuilder::new()
+            .server_uri(config.mqtt.host.clone())
+            .client_id(config.mqtt.client_id.clone())
+            .finalize();
+
+        let mqtt_client = mqtt::AsyncClient::new(create_opts).unwrap_or_else(|e| {
+            println!("Error creating the client: {:?}", e);
+            process::exit(1);
+        });
+
+        let influxdb_client = influxdb2::Client::new(
+            config.influxdb2.host.clone(),
+            config.influxdb2.org.clone(),
+            config.influxdb2.token.clone(),
+        );
+        Self {
+            config,
+            influxdb_client,
+            mqtt_client,
+        }
+    }
+    
+    fn reload(self, new_config: Config) -> Self {
+        // Unsubscribe from old topics
+        self.mqtt_client.disconnect(None);
+        
+        drop(self.influxdb_client);
+
+        Self::new(new_config)
+    }
+}
+
+fn topic_subscriptions(config: &MqttConfig) -> Vec<String> {
+    config
+        .topics
+        .iter()
+        .map(|topic| topic.name.clone())
+        .collect::<Vec<_>>()
+}
+
 #[tokio::main]
 async fn main() -> Result<(), Box<dyn std::error::Error>> {
     // Influxdb2
 
-    let config = Config::from_file("./config.toml").expect("Couldn't load config");
+    let initial_config = Config::from_file("./config.toml").expect("Couldn't load config");
 
-    let influx_client = influxdb2::Client::new(
-        config.influxdb2.host,
-        config.influxdb2.org,
-        config.influxdb2.token,
-    );
-    // MQTT
+    let state = Arc::new(Mutex::new(AppState::new(initial_config)));
 
-    let create_opts = mqtt::CreateOptionsBuilder::new()
-        .server_uri(config.mqtt.host.clone())
-        .client_id(config.mqtt.client_id.clone())
-        .finalize();
-
-    let mut cli = mqtt::AsyncClient::new(create_opts).unwrap_or_else(|e| {
-        println!("Error creating the client: {:?}", e);
-        process::exit(1);
-    });
+    // Init
+    let mut st = state.lock().await;
 
     // Get message stream before connecting.
-    let mut strm = cli.get_stream(25);
+    let mut strm = st.mqtt_client.get_stream(25);
 
     // Define the set of options for the connection
     let lwt = mqtt::Message::new("test", "Async subscriber lost connection", mqtt::QOS_1);
@@ -99,24 +136,23 @@ async fn main() -> Result<(), Box<dyn std::error::Error>> {
 
     // Make the connection to the broker
     println!("Connecting to the MQTT server...");
-    cli.connect(conn_opts).await?;
+    st.mqtt_client.connect(conn_opts).await?;
 
-    let subscriptions = config
-        .mqtt
-        .topics
-        .iter()
-        .map(|topic| topic.name.clone())
-        .collect::<Vec<_>>();
+    let subscriptions = topic_subscriptions(&st.config.mqtt);
 
     println!("Subscribing to topics: {:?}", subscriptions);
-    cli.subscribe_many(
-        &subscriptions,
-        &subscriptions.iter().map(|_| 1i32).collect::<Vec<_>>(),
-    )
-    .await?;
+    st.mqtt_client
+        .subscribe_many(
+            &subscriptions,
+            &subscriptions.iter().map(|_| 1i32).collect::<Vec<_>>(),
+        )
+        .await?;
 
     // Just loop on incoming messages.
     println!("Waiting for messages...");
+    
+    // release AppState lock
+    drop(st);
 
     // Note that we're not providing a way to cleanly shut down and
     // disconnect. Therefore, when you kill this app (with a ^C or
@@ -124,17 +160,18 @@ async fn main() -> Result<(), Box<dyn std::error::Error>> {
     // should emit the LWT message.
 
     while let Some(msg_opt) = strm.next().await {
+        let st = state.lock().await;
         if let Some(msg) = msg_opt {
-            let points = vec![to_data_point(&msg, &config.mqtt)];
+            let points = vec![to_data_point(&msg, &st.config.mqtt)];
 
-            influx_client
-                .write(&config.influxdb2.bucket, futures::stream::iter(points))
+            st.influxdb_client
+                .write(&st.config.influxdb2.bucket, futures::stream::iter(points))
                 .await
                 .unwrap();
         } else {
             // A "None" means we were disconnected. Try to reconnect...
             println!("Lost connection. Attempting reconnect.");
-            while let Err(err) = cli.reconnect().await {
+            while let Err(err) = st.mqtt_client.reconnect().await {
                 println!("Error reconnecting: {}", err);
                 // For tokio use: tokio::time::delay_for()
                 tokio::time::sleep(Duration::from_millis(1000)).await;
