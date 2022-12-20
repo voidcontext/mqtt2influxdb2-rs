45914052fb6afb3570f3ae40d2336ac3718dc514
Gabor Pihaj <gabor.pihaj@gmail.com>
2022-12-20 21:43:21 +0000

    Upgrade nix-rust-utils flake

--- flake.nix
+++ flake.nix
@@ -1,6 +1,6 @@
 {
+  inputs.nru.url = "github:voidcontext/nix-rust-utils?refs/heads/tags=v0.1.1+rust-1.66.0";
 
-  inputs.nru.url = "github:voidcontext/nix-rust-utils";
   outputs = { nru, ...}: 
     nru.lib.mkOutputs {
       src = ./.;
